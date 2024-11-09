3d453815bfa6bdf48aed286cae6dc24680060a4a
Daniel N. Hansten <dnh2000@gmail.com>
2024-11-09 16:56:56 +0100

    funker bra

--- src/main.rs
+++ src/main.rs
@@ -1,298 +1,87 @@
-// industrial-io/examples/riio_bufavg.rs
-//
-// Simple Rust IIO example for buffered reading and post-processing.
-// This does buffered reading with a trigger, then sends the data
-// to a second thread to convert and process.
-//
-// For the sake of simplicity, we assume a raw sample type of a signed,
-// 16-bit integer. A real, dedicated application might do something similar,
-// but a general-purpose solution would probe the channel type and/or use
-// generics to read and convert the raw data.
-//
-// For quick tests, just set `RawSampleType` to the type matchine the channel
-// to be tested.
-//
-// Copyright (c) 2019, Frank Pagliughi
-//
-// Licensed under the MIT license:
-//   <LICENSE or http://opensource.org/licenses/MIT>
-// This file may not be copied, modified, or distributed except according
-// to those terms.
-//
-
-use anyhow::{bail, Context, Result};
-use chrono::offset::Utc;
-use chrono::DateTime;
-
 use industrial_io as iio;
-use std::{
-    any::TypeId,
-    cmp, process,
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        mpsc::{channel, Receiver, SendError, Sender},
-        Arc,
-    },
-    thread::{spawn, JoinHandle},
-    time::{Duration, SystemTime, UNIX_EPOCH},
-    u8,
-};
+use std::{process, time::Duration};
 
-// The type to use for raw samples.
-type RawSampleType = i16;
-
-// Time-stamped data buffer
-type TsDataBuffer = (u64, Vec<RawSampleType>);
-
-const DFLT_FREQ: i64 = 100;
-const DFLT_NUM_SAMPLE: usize = 100;
-
-const SAMPLING_FREQ_ATTR: &str = "sampling_frequency";
-
-/////////////////////////////////////////////////////////////////////////////
-
-// Active data processing object.
-// Each one of these has a thread that can process a buffer's worth of
-// incoming data at a time.
-struct Averager {
-    sender: Sender<TsDataBuffer>,
-    thr: JoinHandle<()>,
-}
-
-impl Averager {
-    // Creates a new averager with the specified offset and scale.
-    pub fn new(offset: f64, scale: f64) -> Self {
-        let (sender, receiver) = channel();
-        let thr = spawn(move || Self::thread_func(receiver, offset, scale));
-        Self { sender, thr }
-    }
-
-    // The internal thread function.
-    // This just loops, receiving buffers of data, then averages them,
-    // transforming to physical units like Volts, deg C, etc, and then
-    // prints them to stdout.
-    fn thread_func(receiver: Receiver<TsDataBuffer>, offset: f64, scale: f64) {
-        loop {
-            let (ts, data): TsDataBuffer = receiver.recv().unwrap();
-
-            if data.is_empty() {
-                break;
-            }
-
-            // Print the timestamp as the UTC time w/ millisec precision
-            let sys_tm = SystemTime::UNIX_EPOCH + Duration::from_nanos(ts);
-            let dt: DateTime<Utc> = sys_tm.into();
-            print!("{}: ", dt.format("%T%.6f"));
-
-            // Compute the average, then scale the result.
-            let sum: f64 = data.iter().map(|&x| f64::from(x)).sum();
-            let avg = sum / data.len() as f64;
-            let val = (avg + offset) * scale / 1000.0;
-
-            // Print out the scaled average, along with
-            // the first few raw values from the buffer
-            println!("<{:.2}> - {:?}", val, &data[0..4]);
-        }
-    }
-
-    // Send data to the thread for processing
-    pub fn send(&self, data: TsDataBuffer) -> Result<(), SendError<TsDataBuffer>> {
-        self.sender.send(data)
-    }
-
-    // Tell the inner thread to quit, then block and wait for it.
-    pub fn quit(self) {
-        self.sender.send((0, vec![])).unwrap();
-        self.thr.join().unwrap();
-    }
-}
-
-/////////////////////////////////////////////////////////////////////////////
-
-// If the IIO device doesn't have a timestamp channel, we can use this to
-// get an equivalent, though less accurate, timestamp.
-pub fn timestamp() -> u64 {
-    let ts = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Clock error")
-        .as_secs_f64();
-    (1.0e9 * ts) as u64
-}
-
-/////////////////////////////////////////////////////////////////////////////
-
-fn run() -> Result<()> {
-    let dev_name = "iio:device0";
-
-    let ctx = iio::Context::new().expect("Couldn't open IIO context.");
-
-    let dev = ctx
-        .find_device(dev_name)
-        .expect(format!("No IIO device named '{}'", dev_name).as_str());
-    println!("channels in dev: {}", dev.num_channels());
-    for chan in 0..=1 {
-        if let Ok(chan) = dev.get_channel(chan) {
-            chan.enable();
-        }
-    }
-
-    println!("Using device: {}", dev_name);
-
-    // ----- Find the timestamp channel (if any) and a data channel -----
-    let ch0 = dev.get_channel(0).unwrap();
-    let _ch1 = dev.get_channel(1).unwrap();
-    let mut ts_chan = dev.find_channel("timestamp", false);
-
-    if ts_chan.is_some() {
-        println!("Found timestamp channel.");
-    } else {
-        println!("No timestamp channel. Estimating timestamps.");
-    }
-
-    println!("Using channel: {}", ch0.name().unwrap_or("??".to_string()));
-
-    // if ch0.type_of() != Some(TypeId::of::<RawSampleType>()) {
-    //     bail!(
-    //         "The channel type ({:?}) is different than expected.",
-    //         ch0.type_of()
-    //     );
-    // }
-    let u8_type = TypeId::of::<u8>();
-    let i8_type = TypeId::of::<i8>();
-    let u16_type = TypeId::of::<u16>();
-    let i16_type = TypeId::of::<i16>();
-    let u32_type = TypeId::of::<u32>();
-    let i32_type = TypeId::of::<i32>();
-    let f32_type = TypeId::of::<f32>();
-    let f64_type = TypeId::of::<f64>();
-
-    match ch0.type_of() {
-        Some(t) if t == u8_type => println!("u8"),
-        Some(t) if t == i8_type => println!("i8"),
-        Some(t) if t == u16_type => println!("u16"),
-        Some(t) if t == i16_type => println!("i16"),
-        Some(t) if t == u32_type => println!("u32"),
-        Some(t) if t == i32_type => println!("i32"),
-        Some(t) if t == f32_type => println!("f32"),
-        Some(t) if t == f64_type => println!("f64"),
-        _ => bail!(
-            "The channel type ({:?}) is different than expected.",
-            ch0.type_of()
-        ),
-    }
-
-    if let Some(ref mut chan) = ts_chan {
-        chan.enable();
-    }
-
-    ch0.enable();
-
-    // ----- Check for a scale and offset -----
-
-    let offset: f64 = ch0.attr_read_float("offset").unwrap_or(0.0);
-    let scale: f64 = ch0.attr_read_float("scale").unwrap_or(1.0);
-
-    println!("  Offset: {:.3}, Scale: {:.3}", offset, scale);
-
-    // ----- Set sample frequency and trigger -----
-
-    let freq = DFLT_FREQ;
-
-    // If the user asked for a trigger device, see if we can use it
-    if dev.has_attr(SAMPLING_FREQ_ATTR) {
-        // Try to set the sampling rate on the device itself, if supported
-        dev.attr_write(SAMPLING_FREQ_ATTR, freq).with_context(|| {
-            format!(
-                "Can't set sampling rate to {}Hz on {}",
-                freq,
-                dev.name().unwrap()
-            )
-        })?;
-    } else {
-        bail!("No suitable trigger device found");
-    }
-
-    // ----- Create a buffer -----
-
-    let n_sample = DFLT_NUM_SAMPLE;
-
-    let mut buf = dev
-        .create_buffer(n_sample, false)
-        .context("Unable to create buffer")?;
-
-    // Make sure the timeout is more than enough to gather each buffer
-    // Give 50% extra time, or at least 5sec.
-    let ms = cmp::max(5000, 1500 * (n_sample as u64) / (freq as u64));
-    if let Err(err) = ctx.set_timeout_ms(ms) {
-        eprintln!("Error setting timeout of {}ms: {}", ms, err);
-    }
-
-    // ----- Create the averager -----
-
-    let avg = Averager::new(offset, scale);
-
-    // ---- Handle ^C since we want a graceful shutdown -----
-
-    let quit = Arc::new(AtomicBool::new(false));
-    // let q = quit.clone();
-
-    // ----- Capture data into the buffer -----
-
-    println!("Started capturing data...");
-
-    while !quit.load(Ordering::SeqCst) {
-        buf.refill().context("Error filling the buffer")?;
-
-        // Get the timestamp. Use the time of the _last_ sample.
-
-        let ts: u64 = if let Some(ref chan) = ts_chan {
-            buf.channel_iter::<u64>(chan)
-                .nth(n_sample - 1)
-                .unwrap_or_default()
-        } else {
-            timestamp()
-        };
-
-        // Extract and convert the raw data from the buffer.
-        // This puts the raw samples into host format (fixes "endiness" and
-        // shifts into place), but it's still raw data. The other thread
-        // will apply the offset and scaling.
-        // We do this here because the channel is not thread-safe.
-
-        /*
-        Note: We could do the following to convert each sample, one at a time,
-            but it's more efficient to convert the whole buffer using read()
-
-        let data: Vec<RawSampleType> = buf.channel_iter::<RawSampleType>(&sample_chan)
-                                           .map(|x| sample_chan.convert(x))
-                                           .collect();
-        */
-
-        let data: Vec<RawSampleType> = match ch0.read(&buf) {
-            Ok(v) => v,
-            Err(err) => {
-                eprintln!("Error reading data: {}", err);
-                break;
-            }
-        };
-
-        avg.send((ts, data)).unwrap();
-    }
-
-    // ----- Shut down -----
-
-    println!("\nExiting...");
-    avg.quit();
-    println!("Done");
-
-    Ok(())
-}
-
-// --------------------------------------------------------------------------
+const DFLT_DEV_NAME: &str = "iio:device0";
+const SAMPLETIME: Duration = Duration::from_millis(1000);
 
 fn main() {
-    if let Err(err) = run() {
-        eprintln!("{:#}", err);
+    let dev_name = DFLT_DEV_NAME;
+
+    let ctx = iio::Context::new().unwrap_or_else(|_err| {
+        println!("Couldn't open IIO context.");
         process::exit(1);
+    });
+
+    let dev = ctx.find_device(dev_name).unwrap_or_else(|| {
+        println!("No IIO device named '{}'", dev_name);
+        process::exit(2);
+    });
+
+    println!("channels in dev: {}", dev.num_channels());
+    let ch0 = dev.get_channel(0).unwrap();
+    ch0.enable();
+    let mut buf = dev.create_buffer(512, false).unwrap_or_else(|err| {
+        eprintln!("Unable to create buffer: {}", err);
+        process::exit(3);
+    });
+
+    let mut avgs = Vec::new();
+    let mut start = std::time::Instant::now();
+    let mut accum = 0.0;
+    loop {
+        let mut max = 0;
+        let mut min = 4095;
+        while start + SAMPLETIME > std::time::Instant::now() {
+            if let Err(err) = buf.refill() {
+                eprintln!("Error filling the buffer: {}", err);
+                process::exit(4);
+            }
+            let data = buf.channel_iter::<u16>(&ch0).collect::<Vec<u16>>();
+            let avg = data.iter().fold(0, |acc: u64, x| acc + *x as u64) / data.len() as u64;
+            min = *std::cmp::min(data.iter().min().unwrap(), &min);
+            max = *std::cmp::max(data.iter().max().unwrap(), &max);
+            avgs.push(avg);
+        }
+        start += SAMPLETIME;
+        let maxcurrent = adc2current(max as u64);
+        let mincurrent = adc2current(min as u64);
+        let avgcurrent = adc2current(avgs.iter().sum::<u64>() / avgs.len() as u64);
+        accum += (avgcurrent as f64) / 3600.0 / 1000.0;
+        println!(
+            "{:.2} mAh, avg: {:.2} mA, max: {:.2} mA, min: {:.2} mA",
+            accum,
+            avgcurrent as f32 / 1000.0,
+            maxcurrent as f32 / 1000.0,
+            mincurrent as f32 / 1000.0
+        );
+        avgs.clear();
+    }
+}
+
+// ZXCT1009 amp = 10mA = 104uA over 10K resistor = 1040mV
+fn adc2current(adc: u64) -> u64 {
+    let vref = 1800;
+    let adc = adc as u64 * 1000;
+    let adc_max = 4095;
+    let current = (adc * vref / adc_max) / 104;
+    current
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_adc2current() {
+        assert_eq!(adc2current(0), 0);
+        assert_eq!(adc2current(2366), 10_000); // 10mA
+        assert_eq!(adc2current(4095), 17_307); // Max
+    }
+    #[test]
+    fn test_currentEstimate() {
+        let mut accum = 0.0;
+        let mut avgs = vec![2366, 2366, 2366, 2366, 2366];
+        let avgcurrent = adc2current(avgs.iter().sum::<u64>() / avgs.len() as u64);
+        accum += (avgcurrent as f64) / 3600.0 / 1000.0;
+        assert_eq!(accum, 0.0005);
     }
 }
